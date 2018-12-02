cfd10016b3a22073ba046321e68a301d8212a1fa
Đỗ Tường Đại <44903756+tuongdai252@users.noreply.github.com>
2018-12-02 12:36:34 +0700

    Add files via upload

--- js/Scroll_on_Top.js
+++ js/Scroll_on_Top.js
@@ -4,10 +4,14 @@ window.onscroll = function() {
 }
 
 function Scroll_Function() {
-	if (document.body.scrollTop>10 || document.documentElement.scrollTop>10)
+	if (document.body.scrollTop>10 || document.documentElement.scrollTop>10) {
 		document.getElementById("Bam_len_top").style.display = "block";
-	else
+		document.getElementById("Trang_gio_hang").style.display = "block";
+	}
+	else {
 		document.getElementById("Bam_len_top").style.display = "none";
+		document.getElementById("Trang_gio_hang").style.display = "none";
+	}
 }
 
 function Dua_len_dau() {
---
+++ js/sanpham.js
@@ -0,0 +1,36 @@
+function sanpham(masp, tensp, dongvat, theloai, gia, hinh) {
+    this.masp = masp;
+    this.tensp = tensp;
+    this.dongvat = dongvat;
+    this.theloai = theloai;
+    this.gia = gia;
+    this.hinh = hinh;
+}
+
+var thucan = new Array();
+
+
+
+var quanao = new Array();
+
+
+
+var vongco = new Array();
+
+
+
+var vatdung = new Array();
+
+
+
+var suatam = new Array();
+
+
+
+var dochoi = new Array();
+
+
+
+var chuong = new Array();
+
+chuong[0] = new sanpham("PETSSNDM", "Nhà đầu mèo", "cho", "chuong", 338000, "../images/120_2271_20431239_341674479620345_7843554253853490907_n.jpg");
