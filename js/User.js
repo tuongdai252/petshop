981378f42f1c9a88ca4901d4698f22de111573c5
Đỗ Tường Đại <44903756+tuongdai252@users.noreply.github.com>
2018-12-02 14:25:40 +0700

    Add files via upload

---
+++ js/User.js
@@ -0,0 +1,61 @@
+// JavaScript Document
+
+function User() {
+	var ngay = document.getElementById("ngay").value;
+	var thang = document.getElementById("thang").value;
+	var nam = document.getElementById("nam").value;
+	var people = {
+		Ten_TK: document.getElementById("UserName").value,
+		Mat_Khau: document.getElementById("MatKhau").value,
+		Ten_NguoiDung: document.getElementById("Name").value,
+		Gioi_Tinh: document.getElementById("sex").value,
+		Ngay_Sinh: ngay + '/' + thang + '/' + nam,
+		Sdt: document.getElementById("phone").value,
+		CMND: document.getElementById("cmnd").value,
+		Dia_Chi: document.getElementById("home").value,
+		Trang_Thai: 0
+	};
+	localStorage.setItem('Tài khoản', JSON.stringify(people));
+	alert("Bạn đã đăng ký thành công");
+	window.location = 'index.html';
+}
+
+function SignIn () {
+	if(localStorage.getItem('Tài khoản')) {
+		var n = document.getElementById("Ten_dang_nhap").value;
+		var p = document.getElementById("Mat_khau").value;
+		var tk = localStorage.getItem('Tài khoản');
+		tk = JSON.parse(tk);
+		if (n==tk.Ten_TK && p==tk.Mat_Khau) {
+			tk.Trang_Thai = 1;
+			localStorage.setItem('Tài khoản',JSON.stringify(tk));
+			alert("Đăng nhập thành công!!!!");
+			window.localStorage='index.html';
+		}
+		else
+			alert("Tài khoản không tồn tại!!!!!!!!");
+	}
+	else
+		alert("Tài khoản không tồn tại!!!!!!!!");
+}
+
+function Check_Tai_Khoan() {
+	var dang_nhap = document.getElementById('login');
+	var dang_ki = document.getElementById('signup');
+	if(localStorage.getItem('Tài khoản')) {
+		var tk = JSON.parse(localStorage.getItem('Tài khoản'));
+		if(tk.Trang_Thai==1) {
+			dang_ki.innerHTML = '<i class="fas fa-user-tie"></i> ' + tk.Ten_NguoiDung;
+			dang_ki.style.color = 'red';
+			dang_ki.style.font = '20px';
+			dang_ki.style.fontWeight= 'bold';
+			dang_nhap.innerHTML = '<button>Đăng xuất<button>';
+			dang_nhap.setAttribute('href','');
+			dang_nhap.addEventListener('click',function(e) {
+				tk.Trang_Thai=0;
+				localStorage.setItem('Tài khoản',JSON.stringify(tk));
+				location.reload();	
+			});
+		}
+	}
+}
--- js/sanpham.js
+++ js/sanpham.js
@@ -7,30 +7,23 @@ function sanpham(masp, tensp, dongvat, theloai, gia, hinh) {
     this.hinh = hinh;
 }
 
-var thucan = new Array();
+var cho = new Array();
+
+cho[0] = new sanpham("PETSSNDM", "Nhà đầu mèo", "cho", "chuong", 338000, "../images/120_2271_20431239_341674479620345_7843554253853490907_n.jpg");
+
+
+var meo = new Array();
 
 
 
-var quanao = new Array();
+var chim = new Array();
 
 
 
-var vongco = new Array();
+var ca = new Array();
 
 
 
-var vatdung = new Array();
+var hamster = new Array();
 
 
-
-var suatam = new Array();
-
-
-
-var dochoi = new Array();
-
-
-
-var chuong = new Array();
-
-chuong[0] = new sanpham("PETSSNDM", "Nhà đầu mèo", "cho", "chuong", 338000, "../images/120_2271_20431239_341674479620345_7843554253853490907_n.jpg");
